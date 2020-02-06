26488711ef9d70b95c5ce953bf484d28f1065c31
ReidConsole <jermainereid@mec.science>
2020-02-06 14:04:13 -0500

    Classwork Mod

--- Classwork/Lecture03/main.cpp
+++ Classwork/Lecture03/main.cpp
@@ -3,21 +3,44 @@
 #include <ctime>
 #include "Array.h"
 
-int main()
+void RandInts(ds::Array<int>& arr,int lo,int hi)
 {
-    ds::Array<int> a(10);
-    ds::it::Iterator<int>* iter = a.ToIterator();
-
-    srand(time(NULL));
+    int mx = (hi >= lo)?(hi):(lo), mn = (lo <= hi)?(lo):(hi);
+    ds::it::Iterator<int>* iter = arr.ToIterator();
 
     while(iter->HasNext())
     {
-        iter->Next() = rand() % 30 + 1;
+        iter->Next() = rand() % (mx - mn + 1) + mn;
     }
 
     delete iter;
-    
-    std::cout << a << "\n";
+} 
 
+void SortedInts(ds::Array<int>& arr,int start)
+{
+    int prev = start;
+    ds::it::Iterator<int>* iter = arr.ToIterator();
+
+    while(iter->HasNext())
+    {
+        prev = prev + rand() % 10;
+        iter->Next() = prev; 
+    }
+
+    delete iter;
+}
+
+int main()
+{
+    ds::Array<int> a(10), b(20);
+
+    srand(time(NULL));
+
+    RandInts(a,1,50);
+    SortedInts(b,8);
+
+    std::cout << a << "\n";
+    std::cout << b << "\n";
+    
     return 0; 
 }
