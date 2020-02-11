7c1850c9bd44c9fe1a22a896102ae91ee229975f
ReidConsole <jermainereid@mec.science>
2020-02-11 13:56:54 -0500

    Labs Mod

---
+++ Labs/Lab05/lab05.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Array.h"
+
+void RandInts(ds::Array<int>& arr,int lo,int hi)
+{
+    int mx = (hi >= lo)?(hi):(lo);
+    int mn = (lo <= hi)?(lo):(hi);
+    ds::it::Iterator<int>* iter = arr.ToIterator();
+
+    while(iter->HasNext())
+    {
+        iter->Next() = rand() % (mx - mn + 1) + mn;
+    }
+
+    delete iter;
+} 
+
+void SortedInts(ds::Array<int>& arr,int start,bool dup)
+{
+    int prev = start;
+    int c = (dup)?(0):(1);
+    ds::it::Iterator<int>* iter = arr.ToIterator();
+
+    while(iter->HasNext())
+    {
+        prev = prev + rand() % 10 + c;
+        iter->Next() = prev; 
+    }
+
+    delete iter;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    return 0; 
+}
