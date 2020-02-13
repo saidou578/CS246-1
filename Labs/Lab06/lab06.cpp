ed537a445d020f59ae3ea51c910f5b6dd42a54a8
ReidConsole <jermainereid@mec.science>
2020-02-13 12:32:59 -0500

    Labs Mod

---
+++ Labs/Lab06/lab06.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Array.h"
+
+/**********************************
+ * Question 1: Swap Counts
+ * a. 
+ * b. 
+ * c.
+ **********************************/
+
+template <typename T>
+bool Monotonic(const Array<T>& data)
+{
+    return false;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    return 0; 
+}
