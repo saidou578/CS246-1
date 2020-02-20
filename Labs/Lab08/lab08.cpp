941b4e4e0fec97515f02fc7b78e41d5677a7e3c0
ReidConsole <jermainereid@mec.science>
2020-02-20 10:57:07 -0500

    Labs Mod

---
+++ Labs/Lab08/lab08.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Node.h"
+#include "Iterator.h"
+
+template <typename T>
+void SwapNodes(ds::Node<T>* root,ds::Node<T>* nptr1,ds::Node<T>* nptr2)
+{
+    return;
+}
+
+double SecondLargest(ds::Node<double>* root)
+{
+    return 0.0;
+}
+
+template <typename T>
+void RemoveAll(ds::Node<T>* root,const T& target)
+{
+    return;
+}
+
+int main()
+{
+    return 0; 
+}
